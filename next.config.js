eedabfc670196e16d983bd985fce76b5de12dc4a
Souphiane Jender <85786227+SouphianeJ@users.noreply.github.com>
2025-03-17 21:53:45 +0100

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -2,9 +2,23 @@
 const nextConfig = {
   reactStrictMode: true,
   // Activer si vous utilisez des images externes
+async headers() {
+    return [
+      {
+        source: '/content/questions.json',
+        headers: [
+          {
+            key: 'Cache-Control',
+            value: 'public, max-age=3600, stale-while-revalidate=86400'
+          }
+        ]
+      }
+    ];
+  },
   images: {
     domains: ['vercel.com'],
   },
 }
 
 module.exports = nextConfig
+
