5600b375778815bc9e23f4e83821077d4ceb6cc6
Souphiane Jender <85786227+SouphianeJ@users.noreply.github.com>
2025-03-18 01:19:09 +0100

    Create pages/api/random-question.js

---
+++ pages/api/random-question.js
@@ -0,0 +1,13 @@
+import path from 'path';
+import fs from 'fs';
+
+export default function handler(req, res) {
+  const filePath = path.join(process.cwd(), 'public', 'content', 'questions.json');
+  const fileContents = fs.readFileSync(filePath, 'utf8');
+  const questions = JSON.parse(fileContents);
+
+  const randomIndex = Math.floor(Math.random() * questions.length);
+  const randomQuestion = questions[randomIndex];
+
+  res.status(200).json(randomQuestion);
+}
